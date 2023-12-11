1689293341eaba9534de40fd76ab0ba9702571fc
VIrtuaT <fcnts3@gmail.com>
2023-12-11 14:37:22 +0000

    move function improving

--- Brownian_motion.py
+++ Brownian_motion.py
@@ -31,13 +31,32 @@ class Particle:
         self.speed = START_SPEED
         self.angle = START_ANGLE
         self.is_tracer = is_tracer
-        self.path
+        self.path = []
 
     def move(self):
-        START_POSITION = [[self.x, self.y]]
+        START_POSITION = [self.x, self.y]
         START_x_VELOCITY = START_SPEED * math.cos(START_ANGLE)
         START_y_VELOCITY = START_SPEED * math.sin(START_ANGLE)
-        current_position = START_POSITION
+        current_velocity = START_SPEED
+        current_x_velocity = START_x_VELOCITY
+        current_y_velocity = START_y_VELOCITY
+        current_position = [START_POSITION[0] + START_x_VELOCITY, START_POSITION[1] + START_y_VELOCITY]
+        current_angle = START_ANGLE
+
+        if self.check_collision(self, other_particle): 
+            # you can find these expressions in collisions wikipedia, simplified since m1 = m2
+            contact_angle = 0          
+            velocity2 = 0  #other_particle velocity
+            velocity1 = current_velocity
+            angle1 = current_angle
+            angle2 = 0 #other_particle angle
+            current_x_velocity = (velocity2*math.cos(angle2 - contact_angle))*math.cos(contact_angle) + velocity1*math.sin(angle1 - contact_angle)*math.cos(contact_angle+(PI/2))
+            current_y_velocity = (velocity2*math.cos(angle2 - contact_angle))*math.sin(contact_angle) + velocity1*math.sin(angle1 - contact_angle)*math.sin(contact_angle+(PI/2))
+            
+        return current_position
+
+
+
 
 
 
@@ -49,13 +68,13 @@ class Particle:
     def particle_collision(self):
         pass
     def check_collision(self, other_particle):
-        return
+        return True
     
 # Create particles
 particles = []
 
 for i in (0, NUM_PARTICLES):
-    particles.append([random.uniform(0, WIDTH), random.uniform(0, HEIGHT), False])
+    particles.append(Particle(random.uniform(0, WIDTH), random.uniform(0, HEIGHT), False))
 
 # Choose one particle as a tracer
 tracer_index = random.randint(0, NUM_PARTICLES - 1) 
